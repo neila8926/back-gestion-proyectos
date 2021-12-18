a3b623334500cb7b11f81d160f0e179ff365c08b
Neyla Polo Villanueva <neila8926@hotmail.com>
2021-12-18 06:49:06 -0500

    mejoras

--- routes/proyecto.route.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { crearProyecto, getProyectos, updateProyecto } = require('../controllers/proyecto.controllers');
-const router = Router();
-
-router.post('/api/proyectos', crearProyecto)
-router.get('/api/proyectos', getProyectos)
-router.put('/api/proyectos/:id', updateProyecto)
-
-module.exports = router;
